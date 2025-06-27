d31c16d6d9e423f0492880557c31460aba2bd6c7
NitinAlphaseam <nitinsonawane@alphaseam.com>
2025-06-27 17:02:03 +0530

    Login Api Integration

--- src/services/billingService.js
+++ src/services/billingService.js
@@ -1,6 +1,6 @@
-import axios from "axios";
+import api from "./api";
 
-export const getBillById = (billId) => axios.get(`/api/bill/${billId}`);
+export const getBillById = (billId) => api.get(`/api/bill/${billId}`);
 
 export const updateBill = (billId, updatedData) =>
-  axios.put(`/api/bill/${billId}`, updatedData);
+  api.put(`/api/bill/${billId}`, updatedData);
